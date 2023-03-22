4a5bc718fa84aeb36ccc89b328195d5fe23564c7
Andres Villca <andres24vt@gmail.com>
2023-03-22 18:29:48 +0100

    index.php
    Se ha creado la pagina de index.php

--- miniGamesProject/resources/views/welcome.blade.php
+++ miniGamesProject/resources/views/welcome.blade.php
@@ -3,15 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>IETIMINIGAMES</title>
+        <link rel="stylesheet" href="..\css\app.css">
+        @vite(['resources/css/app.css'])
     </head>
-    <body class="antialiased">
-        <div id="app"></div>
-        @vite('resources/js/app.js')
+    <body>
+        <div id="buttonEntrar">
+            <a href="/login">ENTRAR</a>
+        </div>
+        <!-- <div id="app"></div>
+        @vite('resources/js/app.js') -->
     </body>
 </html>
