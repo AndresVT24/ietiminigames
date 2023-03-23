61b231c8b9c2d3d2b7147df0435e45c13dc9649c
Andres Villca <andres24vt@gmail.com>
2023-03-23 19:27:56 +0100

    login css

--- miniGamesProject/resources/views/login.blade.php
+++ miniGamesProject/resources/views/login.blade.php
@@ -5,8 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <h1>Login</h1>
+<body class="bodyLogin">
+    <div class="divForm">
+        <form>
+            <p>Login</p>
+            <hr>
+            <div class="divDadesForm">
+                <div class="labelInput">
+                    <p>NOMBRE DE USURIO: </p>
+                    <input type="text" placeholder="USUARIO">
+                </div>
+                <div class="labelInput">
+                    <p>CONTRASEÑA:</p>
+                    <input type="password">
+                </div>
+                <button type="submit" class="inputEnviar">Enviar</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
